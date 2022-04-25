9262a18452b4ff6a1da5d6a8f827e401d3a687f5
Nidsingh123 <officialnidhi123@gmail.com>
2022-04-25 21:33:18 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import  ChatFeed  from './components/ChatFeed' 
+import LoginForm from './components/LoginForm';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const projectID="e722d15c-1c99-4a6b-bcb9-e6f82aec7863"
+const App=()=>{
+    if(!localStorage.getItem('username')) return <LoginForm/>
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID= {projectID}
+            // userName={"Nidhi"}
+            // userSecret={"09092000"}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}/> }
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+    )
 }
-
 export default App;
